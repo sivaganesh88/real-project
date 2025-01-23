51a7ca8a1b94a64c8886f413af181e1814a095fc
sivaganesh88 <gsivaganesh9342798166@gmail.com>
2025-01-23 20:10:26 +0530

    Fixed ESLint warnings and added alt prop to img elements

--- src/index.js
+++ src/index.js
@@ -2,28 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Checked from './Checked';
-import Error from './Error';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-// Define your routes using `createBrowserRouter`
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <Error />, // Specify the error boundary
-    children: [
-      {
-        path: "checked",
-        element: <Checked />,
-      },
-    ],
-  },
-]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <div className="w-screen h-screen flex justify-center items-center">
-    <RouterProvider router={router} /> {/* Use RouterProvider */}
+  <div className='w-screen h-screen flex justify-center items-center '>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
   </div>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
